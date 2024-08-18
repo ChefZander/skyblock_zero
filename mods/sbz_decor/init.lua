9101b8738838cb80f8ff0ec679d9f858f21774b6
ChefZander <tobias.aha2008@gmail.com>
2024-08-18 20:58:01 +0200

    photonlmap light source 14

--- mods/sbz_decor/init.lua
+++ mods/sbz_decor/init.lua
@@ -1,10 +1,10 @@
 minetest.register_node("sbz_decor:photonlamp", {
-    description = "Photon Lamp\n\nLight Source Only. Strength: 15.",
+    description = "Photon Lamp\n\nLight Source Only. Strength: 14,",
     drawtype = "mesh",
     mesh = "photonlamp.obj",
     tiles = {"photonlamp.png"},
     groups = {matter = 1},
-    light_source = 15,
+    light_source = 14,
     selection_box = {
         type = "fixed",
         fixed = { -0.5, -0.5, -0.5, 0.5, 0.5, 0.5 },
