5b5ede9721e114a52770fe57cd52eb5180df1d51
ChefZander <tobias.aha2008@gmail.com>
2024-08-20 22:01:34 +0200

    fix birdlovers pr

--- mods/sbz_progression/questbook.lua
+++ mods/sbz_progression/questbook.lua
@@ -378,7 +378,7 @@ local function get_questbook_formspec(selected_quest_index, player_name)
         formspec = formspec ..
             "hypertext[6,0.3;100,100;;\\<big\\>" .. minetest.formspec_escape(selected_quest.title) .. "]" ..
             "textarea[6,1.3;5.8,5;;;" ..
-            (is_quest_available(player_name, selected_quest.title) and minetest.formspec_escape(selected_quest.text) or "Complete " .. combineWithAnd(c) .. " to unlock.") ..
+            (is_quest_available(player_name, selected_quest.title) and minetest.formspec_escape(selected_quest.text) or "Complete " .. combineWithAnd(selected_quest.requires) .. " to unlock.") ..
             "]" .. -- minetest.formspec_escape(selected_quest.text)
             "label[6,7.2;" ..
             (is_achievement_unlocked(player_name, selected_quest.title) and "✔️ You have completed this Quest." or "You have not completed this Quest.") ..
