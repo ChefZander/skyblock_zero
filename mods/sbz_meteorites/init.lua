cac827bf14b5df4a9f44ec9ff511b1ca4aca857c
theidealist <theidealistmusic@gmail.com>
2024-08-21 18:36:12 +0100

    meteorites now explode and leave behind mineable blocks

--- mods/sbz_meteorites/init.lua
+++ mods/sbz_meteorites/init.lua
@@ -4,6 +4,104 @@ local function get_nearby_player(pos)
     end
 end
 
+--vector.random_direction was added in 5.10-dev, but I use 5.9, so make sure this exists
+--code borrowed from builtin/vector.lua in 5.10-dev
+if not vector.random_direction then
+    function vector.random_direction()
+        -- Generate a random direction of unit length, via rejection sampling
+        local x, y, z, l2
+        repeat -- expected less than two attempts on average (volume sphere vs. cube)
+            x, y, z = math.random() * 2 - 1, math.random() * 2 - 1, math.random() * 2 - 1
+            l2 = x*x + y*y + z*z
+        until l2 <= 1 and l2 >= 1e-6
+        -- normalize
+        local l = math.sqrt(l2)
+        return vector.new(x/l, y/l, z/l)
+    end
+end
+
+local function meteorite_explode(pos, type)
+    --breaking nodes
+    for _ = 1, 100 do
+        local raycast = minetest.raycast(pos, pos+vector.random_direction()*8, false)
+        local wear = 0
+        for pointed in raycast do
+            if pointed.type == "node" then
+                local nodename = minetest.get_node(pointed.under).name
+                wear = wear+(1/minetest.get_item_group(nodename, "explody"))
+                --the explody group hence signifies roughly how many such nodes in a straight line it can break before stopping
+                --although this is very random
+                if wear > 1 then break end
+                minetest.set_node(pointed.under, {name=minetest.registered_nodes[nodename]._exploded or "air"})
+            end
+        end
+    end
+    --placing nodes
+    local node_types = {matter_blob="sbz_meteorites:meteoric_matter", emitter="sbz_meteorites:meteoric_emittrium"}
+    for _ = 1, 16 do
+        local new_pos = pos+vector.new(math.random(-1, 1), math.random(-1, 1), math.random(-1, 1))
+        if minetest.get_node(new_pos).name == "air" then
+            minetest.set_node(new_pos, {name=node_types[type]})
+        end
+    end
+    --particle effects
+    minetest.add_particlespawner({
+        time = 0.1,
+        amount = 2000,
+        pos = pos,
+        radius = 1,
+        drag = 0.2,
+        glow = 14,
+        exptime = {min=2, max=5},
+        size = {min=3, max=6},
+        texture = "meteorite_trail_"..type..".png",
+        animation = {type="vertical_frames", aspect_width=4, aspect_height=4, length=-1},
+        attract = {
+            kind = "point",
+            origin = pos,
+            strength = {min=-4, max=0}
+        }
+    })
+    local forward = vector.new(1, 0, 0)
+    local up = vector.new(0, 1, 0)
+    for _ = 1, 500 do
+        local dir = vector.rotate_around_axis(forward, up, math.random()*2*math.pi)
+        local expiry = math.random()*3+2
+        minetest.add_particle({
+            pos = pos+dir,
+            velocity = dir*(5+math.random()),
+            drag = vector.new(0.2, 0.2, 0.2),
+            glow = 14,
+            expirationtime = expiry,
+            size = math.random()*3+3,
+            texture = "meteorite_trail_"..type..".png^[colorize:#aaaaaa:alpha",
+            animation = {type="vertical_frames", aspect_width=4, aspect_height=4, length=expiry},
+        })
+    end
+end
+
+minetest.register_node("sbz_meteorites:meteoric_matter", {
+    description = "Meteoric Matter",
+    tiles = {"matter_blob.png^meteoric_overlay.png"},
+    paramtype = "light",
+    light_source = 10,
+    groups = {matter=1, cracky=3},
+    drop = "sbz_resources:matter_dust 9"
+})
+
+minetest.register_node("sbz_meteorites:meteoric_emittrium", {
+    description = "Meteoric Emittrium",
+    tiles = {"emitter.png^meteoric_overlay.png"},
+    paramtype = "light",
+    light_source = 10,
+    groups = {matter=1, cracky=3},
+    drop = "sbz_resources:raw_emittrium 9"
+})
+
+minetest.register_node("sbz_meteorites:meteoric_metal", {
+    
+})
+
 minetest.register_entity("sbz_meteorites:meteorite", {
     initial_properties = {
         visual = "cube",
@@ -32,7 +130,7 @@ minetest.register_entity("sbz_meteorites:meteorite", {
     get_staticdata = function (self)
         return self.type
     end,
-    on_step = function (self, dtime)
+    on_step = function (self, dtime, moveresult)
         local pos = self.object:get_pos()
         local diag = vector.new(1, 1, 1)
         minetest.add_particlespawner({
@@ -47,6 +145,10 @@ minetest.register_entity("sbz_meteorites:meteorite", {
             texture = "meteorite_trail_"..self.type..".png",
             animation = {type="vertical_frames", aspect_width=4, aspect_height=4, length=-1}
         })
+        if moveresult and moveresult.collisions[1] then --colliding with something, should explode
+            self.object:remove()
+            meteorite_explode(pos, self.type)
+        end
     end
 })
 
