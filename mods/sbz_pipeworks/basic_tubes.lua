dfb16614160298b729161715412e9aec2e985d27
TheEt1234 <TheEt1234@users.noreply.github.com>
2024-08-22 18:38:46 +0200

    fix: add broken tube (unusable texture though...)

--- mods/sbz_pipeworks/basic_tubes.lua
+++ mods/sbz_pipeworks/basic_tubes.lua
@@ -88,3 +88,74 @@ pipeworks.register_tube("pipeworks:crossing_tube", {
         tube = { can_go = function(pos, node, velocity, stack) return { velocity } end }
     },
 })
+
+
+pipeworks.register_tube("pipeworks:broken_tube", {
+    description = "Broken Tube",
+    plain = { { name = "pipeworks_broken_tube_plain.png", backface_culling = true, color = "red" } },
+    noctr = { { name = "pipeworks_broken_tube_plain.png", backface_culling = true, color = "red" } },
+    node_def = {
+        drop = "pipeworks:tube_1",
+        groups = { not_in_creative_inventory = 1, tubedevice_receiver = 1 },
+        is_ground_content = false,
+        tube = {
+            insert_object = function(pos, node, stack, direction)
+                minetest.item_drop(stack, nil, pos)
+                return ItemStack("")
+            end,
+            can_insert = function(pos, node, stack, direction)
+                return true
+            end,
+            priority = 50,
+        },
+        on_punch = function(pos, node, puncher, pointed_thing)
+            local itemstack = puncher:get_wielded_item()
+            local wieldname = itemstack:get_name()
+            local playername = puncher:get_player_name()
+            local log_msg = playername .. " struck a broken tube at " .. minetest.pos_to_string(pos) .. "\n            "
+            local meta = minetest.get_meta(pos)
+            local was_node = minetest.deserialize(meta:get_string("the_tube_was"))
+            if not was_node then
+                pipeworks.logger(log_msg .. "but it can't be repaired.")
+                return
+            end
+            if not pipeworks.check_and_wear_hammer(puncher) then
+                if wieldname == "" then
+                    pipeworks.logger(log_msg .. "by hand. It's not very effective.")
+                    if minetest.settings:get_bool("enable_damage") then
+                        minetest.chat_send_player(playername,
+                            S("Broken tubes may be a bit sharp. Perhaps try with a hammer?"))
+                        puncher:set_hp(puncher:get_hp() - 1)
+                    end
+                else
+                    pipeworks.logger(log_msg .. "with " .. wieldname .. " but that tool is too weak.")
+                end
+                return
+            end
+            log_msg = log_msg .. "with " .. wieldname .. " to repair it"
+            local nodedef = minetest.registered_nodes[was_node.name]
+            if nodedef then
+                pipeworks.logger(log_msg .. ".")
+                if nodedef.tube and nodedef.tube.on_repair then
+                    nodedef.tube.on_repair(pos, was_node)
+                else
+                    minetest.swap_node(pos, { name = was_node.name, param2 = was_node.param2 })
+                    pipeworks.scan_for_tube_objects(pos)
+                end
+                meta:set_string("the_tube_was", "")
+            else
+                pipeworks.logger(log_msg .. " but original node " .. was_node.name .. " is not registered anymore.")
+                minetest.chat_send_player(playername, S("This tube cannot be repaired."))
+            end
+        end,
+        allow_metadata_inventory_put = function()
+            return 0
+        end,
+        allow_metadata_inventory_move = function()
+            return 0
+        end,
+        allow_metadata_inventory_take = function()
+            return 0
+        end,
+    }
+})
