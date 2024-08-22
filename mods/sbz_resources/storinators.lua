3a151309b215282a0daf9212f527dc5f0302ef2e
TheEt1234 <TheEt1234@users.noreply.github.com>
2024-08-22 12:08:40 +0200

    Basic item transport - part 1

--- mods/sbz_resources/storinators.lua
+++ mods/sbz_resources/storinators.lua
@@ -26,216 +26,80 @@ local function update_node_texture(pos)
 end
 
 
+for k, v in ipairs({
+    "sbz_resources:storinator",
+    "sbz_resources:storinator_full_1",
+    "sbz_resources:storinator_full_2",
+    "sbz_resources:storinator_full_3",
+}) do
+    local tex = "storinator_empty.png"
 
-minetest.register_node("sbz_resources:storinator", {
-    description = "Storinator\n\nInventory Slots: 30.",
-    tiles = {
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_empty.png",
-    },
-    groups = { matter = 1 },
-    paramtype2 = "facedir",
-    sunlight_propagates = true,
-    walkable = true,
-    on_rightclick = function(pos, node, player, pointed_thing)
-        local player_name = player:get_player_name()
-        minetest.show_formspec(player_name, "sbz_power:simple_charge_generator_formspec",
-            "formspec_version[7]" ..
-            "size[8.2,9]" ..
-            "style_type[list;spacing=.2;size=.8]" ..
-            "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";main;0.2,0.2;8,4;]" ..
-            "list[current_player;main;0.2,5;8,4;]" ..
-            "listring[]")
+    if v ~= "sbz_resources:storinator" then
+        tex = string.sub(v, 15) .. ".png"
+    end
+    minetest.register_node(v, {
+        description = "Storinator\n\nInventory Slots: 30.",
+        tiles = {
+            "storinator_side.png",
+            "storinator_side.png",
+            "storinator_side.png",
+            "storinator_side.png",
+            "storinator_side.png",
+            tex
+        },
+        groups = { matter = 1, tubedevice = 1, tubedevice_receiver = 1, },
+        paramtype2 = "facedir",
+        sunlight_propagates = true,
+        use_texture_alpha = true,
+        on_rightclick = function(pos)
+            -- compat for old worlds, remove after a release or something
+            minetest.get_meta(pos):set_string("formspec", "formspec_version[7]" ..
+                "size[8.2,9]" ..
+                "style_type[list;spacing=.2;size=.8]" ..
+                "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";main;0.2,0.2;8,4;]" ..
+                "list[current_player;main;0.2,5;8,4;]" ..
+                "listring[]")
+        end,
+        on_construct = function(pos)
+            local meta = minetest.get_meta(pos)
+            local inv = meta:get_inventory()
+            inv:set_size("main", 30)
 
-        minetest.sound_play("machine_open", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_construct = function(pos)
-        local meta = minetest.get_meta(pos)
-        local inv = meta:get_inventory()
-        inv:set_size("main", 30)
-
-
-        minetest.sound_play("machine_build", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_metadata_inventory_put = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_take = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
-        update_node_texture(pos)
-    end,
-})
-
-minetest.register_node("sbz_resources:storinator_full_1", {
-    description = "Storinator\n\nInventory Slots: 30.",
-    tiles = {
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_full_1.png",
-    },
-    groups = { matter = 1 },
-    paramtype2 = "facedir",
-    sunlight_propagates = true,
-    walkable = true,
-    on_rightclick = function(pos, node, player, pointed_thing)
-        local player_name = player:get_player_name()
-        minetest.show_formspec(player_name, "sbz_power:simple_charge_generator_formspec",
-            "formspec_version[7]" ..
-            "size[8.2,9]" ..
-            "style_type[list;spacing=.2;size=.8]" ..
-            "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";main;0.2,0.2;8,4;]" ..
-            "list[current_player;main;0.2,5;8,4;]" ..
-            "listring[]")
-
-        minetest.sound_play("machine_open", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_construct = function(pos)
-        local meta = minetest.get_meta(pos)
-        local inv = meta:get_inventory()
-        inv:set_size("main", 30)
-
-
-        minetest.sound_play("machine_build", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_metadata_inventory_put = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_take = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
-        update_node_texture(pos)
-    end,
-})
-
-minetest.register_node("sbz_resources:storinator_full_2", {
-    description = "Storinator\n\nInventory Slots: 30.",
-    tiles = {
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_full_2.png",
-    },
-    groups = { matter = 1 },
-    paramtype2 = "facedir",
-    sunlight_propagates = true,
-    walkable = true,
-    on_rightclick = function(pos, node, player, pointed_thing)
-        local player_name = player:get_player_name()
-        minetest.show_formspec(player_name, "sbz_power:simple_charge_generator_formspec",
-            "formspec_version[7]" ..
-            "size[8.2,9]" ..
-            "style_type[list;spacing=.2;size=.8]" ..
-            "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";main;0.2,0.2;8,4;]" ..
-            "list[current_player;main;0.2,5;8,4;]" ..
-            "listring[]")
-
-        minetest.sound_play("machine_open", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_construct = function(pos)
-        local meta = minetest.get_meta(pos)
-        local inv = meta:get_inventory()
-        inv:set_size("main", 30)
-
-
-        minetest.sound_play("machine_build", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_metadata_inventory_put = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_take = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
-        update_node_texture(pos)
-    end,
-})
-
-minetest.register_node("sbz_resources:storinator_full_3", {
-    description = "Storinator\n\nInventory Slots: 30.",
-    tiles = {
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_side.png",
-        "storinator_full_3.png",
-    },
-    groups = { matter = 1 },
-    paramtype2 = "facedir",
-    sunlight_propagates = true,
-    walkable = true,
-    on_rightclick = function(pos, node, player, pointed_thing)
-        local player_name = player:get_player_name()
-        minetest.show_formspec(player_name, "sbz_power:simple_charge_generator_formspec",
-            "formspec_version[7]" ..
-            "size[8.2,9]" ..
-            "style_type[list;spacing=.2;size=.8]" ..
-            "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";main;0.2,0.2;8,4;]" ..
-            "list[current_player;main;0.2,5;8,4;]" ..
-            "listring[]")
-
-        minetest.sound_play("machine_open", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_construct = function(pos)
-        local meta = minetest.get_meta(pos)
-        local inv = meta:get_inventory()
-        inv:set_size("main", 30)
-
-
-        minetest.sound_play("machine_build", {
-            to_player = player_name,
-            gain = 1.0,
-        })
-    end,
-    on_metadata_inventory_put = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_take = function(pos, listname, index, stack, player)
-        update_node_texture(pos)
-    end,
-    on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
-        update_node_texture(pos)
-    end,
-})
-
-minetest.register_craft({
-    output = "sbz_resources:storinator",
-    recipe = {
-        { "sbz_power:simple_charged_field", "sbz_resources:matter_plate",   "sbz_resources:retaining_circuit" },
-        { "sbz_resources:matter_plate",         "sbz_resources:simple_circuit", "sbz_resources:matter_plate" },
-        { "sbz_resources:retaining_circuit",    "sbz_resources:matter_plate",   "sbz_resources:retaining_circuit" }
-    }
-})
+            meta:set_string("formspec", "formspec_version[7]" ..
+                "size[8.2,9]" ..
+                "style_type[list;spacing=.2;size=.8]" ..
+                "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";main;0.2,0.2;8,4;]" ..
+                "list[current_player;main;0.2,5;8,4;]" ..
+                "listring[]")
+        end,
+        on_metadata_inventory_put = function(pos, listname, index, stack, player)
+            update_node_texture(pos)
+        end,
+        on_metadata_inventory_take = function(pos, listname, index, stack, player)
+            update_node_texture(pos)
+        end,
+        on_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player)
+            update_node_texture(pos)
+        end,
+        drop = "sbz_resources:storinator",
+        tube = {
+            input_inventory = "main",
+            insert_object = function(pos, node, stack, direction)
+                local meta = minetest.get_meta(pos)
+                local inv = meta:get_inventory()
+                return inv:add_item("main", stack)
+            end,
+            can_insert = function(pos, node, stack, direction)
+                local meta = minetest.get_meta(pos)
+                local inv = meta:get_inventory()
+                if meta:get_int("splitstacks") == 1 then
+                    stack = stack:peek_item(1)
+                end
+                return inv:room_for_item("main", stack)
+            end,
+            connect_sides = { left = 1, right = 1, front = 1, back = 1, top = 1, bottom = 1 }
+        },
+        after_dig_node = pipeworks.after_dig,
+        after_place_node = pipeworks.after_place,
+    })
+end
