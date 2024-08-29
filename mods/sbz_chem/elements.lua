ae5ac2eb67d55cd2e3a3d64856309520474cfbf2
TheEt1234 <TheEt1234@users.noreply.github.com>
2024-08-29 19:38:16 +0200

    Add ingots, add high power electric furnace, fix a bug that made one node unbreakable

--- mods/sbz_chem/elements.lua
+++ mods/sbz_chem/elements.lua
@@ -1,101 +1,45 @@
--- Gold (Au)
-minetest.register_craftitem("sbz_chem:gold_powder", {
-    description = "Gold Powder (Au)",
-    inventory_image = "powder.png^[colorize:#FFD700:100",
-})
+sbz_api.register_element = function(name, color, description)
+    minetest.register_craftitem("sbz_chem:" .. name .. "_powder", {
+        groups = { chem_element = 1 },
+        description = string.format(description, "Powder"),
+        inventory_image = "powder.png^[colorize:" .. color .. ":100"
+    })
+    minetest.register_craftitem("sbz_chem:" .. name .. "_ingot", {
+        groups = { chem_element = 1, ingot = 1 },
+        description = string.format(description, "Ingot"),
+        inventory_image = "ingot.png^[colorize:" .. color .. ":100"
+    })
+    minetest.register_craft({
+        type = "cooking",
+        output = "sbz_chem:" .. name .. "_ingot",
+        recipe = "sbz_chem:" .. name .. "_powder",
 
--- Silver (Ag)
-minetest.register_craftitem("sbz_chem:silver_powder", {
-    description = "Silver Powder (Ag)",
-    inventory_image = "powder.png^[colorize:#C0C0C0:100",
-})
+    })
+end
 
--- Iron (Fe)
-minetest.register_craftitem("sbz_chem:iron_powder", {
-    description = "Iron Powder (Fe)",
-    inventory_image = "powder.png^[colorize:#B7410E:100",  -- Darker brownish-red color
-})
 
--- Copper (Cu)
-minetest.register_craftitem("sbz_chem:copper_powder", {
-    description = "Copper Powder (Cu)",
-    inventory_image = "powder.png^[colorize:#B87333:100",  -- Copper color
-})
+sbz_api.register_element("gold", "#FFD700", "Gold %s (Au)")
+sbz_api.register_element("silver", "#C0C0C0", "Silver %s (Ag)")
+sbz_api.register_element("iron", "#B7410E", "Iron %s (Fe)")
+sbz_api.register_element("copper", "#B87333", "Copper %s (Cu)")
+sbz_api.register_element("aluminum", "#A9A9A9", "Aluminum %s (Al)")
+sbz_api.register_element("lead", "#6E6E6E", "Lead %s (Pb)")
+sbz_api.register_element("zinc", "#7F7F7F", "Zinc %s (Zn)")
+sbz_api.register_element("tin", "#D2B48C", "Tin %s (Sn)")
+sbz_api.register_element("nickel", "#A59B91", "Nickel %s (Ni)")
+sbz_api.register_element("platinum", "#E5E4E2", "Platinum %s (Pt)")
+sbz_api.register_element("mercury", "#B5B5B5", "Mercury %s (Hg)")
+sbz_api.register_element("cobalt", "#0047AB", "Cobalt %s (Co)")
+sbz_api.register_element("titanium", "#8A8A8A", "Titanium %s (Ti)")
+sbz_api.register_element("magnesium", "#DADADA", "Magnesium %s (Mg)")
+sbz_api.register_element("calcium", "#F5F5DC", "Calcium %s (Ca)")
+sbz_api.register_element("sodium", "#F4F4F4", "Sodium %s (Na)")
+sbz_api.register_element("lithium", "#BCC6CC", "Lithium %s (Li)")
 
--- Aluminum (Al)
-minetest.register_craftitem("sbz_chem:aluminum_powder", {
-    description = "Aluminum Powder (Al)",
-    inventory_image = "powder.png^[colorize:#A9A9A9:100",  -- Light gray color
-})
+-- alloys
 
--- Lead (Pb)
-minetest.register_craftitem("sbz_chem:lead_powder", {
-    description = "Lead Powder (Pb)",
-    inventory_image = "powder.png^[colorize:#6E6E6E:100",  -- Dark gray color
-})
-
--- Zinc (Zn)
-minetest.register_craftitem("sbz_chem:zinc_powder", {
-    description = "Zinc Powder (Zn)",
-    inventory_image = "powder.png^[colorize:#7F7F7F:100",  -- Medium gray color
-})
-
--- Tin (Sn)
-minetest.register_craftitem("sbz_chem:tin_powder", {
-    description = "Tin Powder (Sn)",
-    inventory_image = "powder.png^[colorize:#D2B48C:100",  -- Light tan color
-})
-
--- Nickel (Ni)
-minetest.register_craftitem("sbz_chem:nickel_powder", {
-    description = "Nickel Powder (Ni)",
-    inventory_image = "powder.png^[colorize:#A59B91:100",  -- Slightly dull metallic color
-})
-
--- Platinum (Pt)
-minetest.register_craftitem("sbz_chem:platinum_powder", {
-    description = "Platinum Powder (Pt)",
-    inventory_image = "powder.png^[colorize:#E5E4E2:100",  -- Light gray-silver color
-})
-
--- Mercury (Hg)
-minetest.register_craftitem("sbz_chem:mercury_powder", {
-    description = "Mercury Powder (Hg)",
-    inventory_image = "powder.png^[colorize:#B5B5B5:100",  -- Slightly darker gray-silver color
-})
-
--- Cobalt (Co)
-minetest.register_craftitem("sbz_chem:cobalt_powder", {
-    description = "Cobalt Powder (Co)",
-    inventory_image = "powder.png^[colorize:#0047AB:100",  -- Dark blue color
-})
-
--- Titanium (Ti)
-minetest.register_craftitem("sbz_chem:titanium_powder", {
-    description = "Titanium Powder (Ti)",
-    inventory_image = "powder.png^[colorize:#8A8A8A:100",  -- Darker gray color
-})
-
--- Magnesium (Mg)
-minetest.register_craftitem("sbz_chem:magnesium_powder", {
-    description = "Magnesium Powder (Mg)",
-    inventory_image = "powder.png^[colorize:#DADADA:100",  -- Light gray color
-})
-
--- Calcium (Ca)
-minetest.register_craftitem("sbz_chem:calcium_powder", {
-    description = "Calcium Powder (Ca)",
-    inventory_image = "powder.png^[colorize:#F5F5DC:100",  -- Beige color
-})
-
--- Sodium (Na)
-minetest.register_craftitem("sbz_chem:sodium_powder", {
-    description = "Sodium Powder (Na)",
-    inventory_image = "powder.png^[colorize:#F4F4F4:100",  -- Light silver color
-})
-
--- Lithium (Li)
-minetest.register_craftitem("sbz_chem:lithium_powder", {
-    description = "Lithium Powder (Li)",
-    inventory_image = "powder.png^[colorize:#BCC6CC:100",  -- Pale gray color
-})
+sbz_api.register_element("bronze", "#CD7F32", "Bronze %s (CuSn)")
+sbz_api.register_element("brass", "#B5A642", "Brass %s (CuZn)")
+sbz_api.register_element("invar", "#808080", "Invar %s (FeNi)")
+sbz_api.register_element("titanium_alloy", "#B0C4DE", "Titanium Alloy %s (TiAl)")
+sbz_api.register_element("white_gold", "#E5E4E2", "White Gold %s (AuNi)")
--- mods/sbz_chem/hpef.lua
+++ mods/sbz_chem/hpef.lua
@@ -1,19 +1,31 @@
-local hpef_recipes = {
-    {recipe = {"sbz_chem:copper_powder"}, output = {"sbz_chem:bronze_powder"}},
-}
+-- theese allow_metadata_* functions were taken from the mtg furnace
+local function allow_metadata_inventory_put(pos, listname, index, stack, player)
+    if listname == "dst" then
+        return 0
+    end
+    return stack:get_count()
+end
 
-sbz_api.register_machine("sbz_chem:high_power_electric_furnace",{
+local function allow_metadata_inventory_move(pos, from_list, from_index, to_list, to_index, count, player)
+    if to_list == "dst" then return 0 end
+    return count
+end
+
+
+sbz_api.register_machine("sbz_chem:high_power_electric_furnace", {
     description = "High Power Electric Furnace",
     tiles = {
-        {name="simple_alloy_furnace.png", animation={type="vertical_frames", length = 0.7}} -- this needs update TODO
+        { name = "simple_alloy_furnace.png", animation = { type = "vertical_frames", length = 0.7 } } -- this needs update TODO
     },
-    groups = {matter =1},
+    groups = { matter = 1 },
+    allow_metadata_inventory_move = allow_metadata_inventory_move,
+    allow_metadata_inventory_put = allow_metadata_inventory_put,
 
     on_construct = function(pos)
         local meta = minetest.get_meta(pos)
         local inv = meta:get_inventory()
-        inv:set_size("input", 1)
-        inv:set_size("output", 1)
+        inv:set_size("src", 1)
+        inv:set_size("dst", 1)
 
 
         minetest.sound_play("machine_build", {
@@ -23,15 +35,17 @@ sbz_api.register_machine("sbz_chem:high_power_electric_furnace",{
     end,
     on_rightclick = function(pos, node, player, pointed_thing)
         local player_name = player:get_player_name()
-        minetest.show_formspec(player_name, "sbz_resources:simple_alloy_furnace_formspec",
-            "formspec_version[7]" ..
-            "size[8.2,9]" ..
-            "style_type[list;spacing=.2;size=.8]" ..
-            "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";input;3.5,1;1,1;]" ..
-            "list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";output;3.5,3;1,1;]" ..
-            "list[current_player;main;0.2,5;8,4;]" ..
-            "listring[]")
+        local meta = minetest.get_meta(pos)
 
+        meta:set_string("formspec", [[
+formspec_version[7]
+size[8.2,9]
+style_type[list;spacing=.2;size=.8]
+list[context;src;3.5,1;1,1;]
+list[context;dst;3.5,3;1,1;]
+list[current_player;main;0.2,5;8,4;]
+listring[]
+    ]])
         minetest.sound_play("machine_open", {
             to_player = player_name,
             gain = 1.0,
@@ -43,30 +57,32 @@ sbz_api.register_machine("sbz_chem:high_power_electric_furnace",{
         local power_needed = 15
         local inv = meta:get_inventory()
 
-        local input = inv:get_stack("input", 1):get_name()
-
-        local selected_item = is_valid_recipe(input_1, input_2)
-        if selected_item == nil then
-            meta:set_string("infotext", "Inactive")
-            return 0
-        end
-
         if demand + power_needed > supply then
             meta:set_string("infotext", "Not enough power")
             return power_needed
         else
             meta:set_string("infotext", "Smelting...")
-            minetest.sound_play({name="simple_alloy_furnace_running", gain=0.6}, {pos=pos})
+            minetest.sound_play({ name = "simple_alloy_furnace_running", gain = 0.6, pos = pos })
 
-            if inv:room_for_item("output", selected_item) then
-                inv:add_item("output", selected_item)
-                inv:remove_item("input", input_1)
-                inv:remove_item("input", input_2)
-            else
-                meta:set_string("infotext", "Output inventory full")
-                return power_needed
+            local src = inv:get_list("src")
+
+            local out, decremented_input = minetest.get_craft_result({
+                method = "cooking",
+                width = 1,
+                items = src,
+            })
+            if out.item:is_empty() then
+                meta:set_string("infotext", "Invalid/no recipe")
+                return 0
             end
 
+            if not inv:room_for_item("dst", out.item) then
+                meta:set_string("infotext", "Full")
+                return 0
+            end
+
+            inv:set_stack("src", 1, decremented_input.items[1])
+            inv:add_item("dst", out.item)
             return power_needed
         end
     end,
@@ -75,8 +91,8 @@ sbz_api.register_machine("sbz_chem:high_power_electric_furnace",{
 minetest.register_craft({
     output = "sbz_chem:simple_alloy_furnace",
     recipe = {
-        { "sbz_power:simple_charged_field", "sbz_resources:antimatter_dust",    "sbz_power:simple_charged_field" },
-        { "sbz_resources:matter_blob",          "sbz_resources:emittrium_circuit", "sbz_resources:matter_blob" },
-        { "sbz_power:simple_charged_field", "sbz_resources:matter_blob",        "sbz_power:simple_charged_field" }
+        { "sbz_power:simple_charged_field", "sbz_resources:antimatter_dust",   "sbz_power:simple_charged_field" },
+        { "sbz_resources:matter_blob",      "sbz_resources:emittrium_circuit", "sbz_resources:matter_blob" },
+        { "sbz_power:simple_charged_field", "sbz_resources:matter_blob",       "sbz_power:simple_charged_field" }
     }
 })
