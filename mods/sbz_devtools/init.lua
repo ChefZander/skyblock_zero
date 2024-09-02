36fbe39210c57baba12d9f820037c94f4d467fa7
ChefZander <tobias.aha2008@gmail.com>
2024-09-02 20:25:29 +0200

    more dev utils yay

--- mods/sbz_devtools/init.lua
+++ mods/sbz_devtools/init.lua
@@ -74,7 +74,7 @@ minetest.register_chatcommand("dev_platform", {
                     end
                 end
             end
-            return true, "platform spawned."
+            return true, "Platform spawned."
         else
             return false, "Player not found."
         end
@@ -91,5 +91,70 @@ minetest.register_chatcommand("dev_trail", {
         local state = ((player:get_meta():get_int("trailHidden") == 1) and 0 or 1)
         player:get_meta():set_int("trailHidden", state)
         return true, "Trail state is "..state
+    end
+})
+
+minetest.register_chatcommand("dev_close", {
+    description = "Get the 8 closest unique nodes within a distance of 32 and receive them as items - for debugging only",
+    privs = { ["server"] = true },
+    func = function(name)
+        local player = minetest.get_player_by_name(name)
+        if not player then
+            return false, "Player not found!"
+        end
+
+        local pos = player:get_pos()
+        local unique_nodes = {}
+        local found_nodes = 0
+
+        for x = -32, 32 do
+            for y = -32, 32 do
+                for z = -32, 32 do
+                    if found_nodes >= 8 then
+                        break
+                    end
+
+                    local node_pos = vector.add(pos, {x = x, y = y, z = z})
+                    local node = minetest.get_node(node_pos)
+                    local node_name = node.name
+
+                    if node_name ~= "air" and not unique_nodes[node_name] then
+                        unique_nodes[node_name] = true
+                        found_nodes = found_nodes + 1
+                        local item_stack = ItemStack(node_name)
+                        player:get_inventory():add_item("main", item_stack)
+                    end
+                end
+                if found_nodes >= 8 then
+                    break
+                end
+            end
+            if found_nodes >= 8 then
+                break
+            end
+        end
+
+        if found_nodes == 0 then
+            return false, "No unique nodes found within 32 blocks."
+        end
+
+        return true, "Given " .. found_nodes .. " unique nodes."
+    end
+})
+
+minetest.register_chatcommand("dev_clear", {
+    description = "Clears the player's inventory - for debugging only",
+    privs = {interact = true},
+    func = function(name)
+        local player = minetest.get_player_by_name(name)
+        if not player then
+            return false, "Player not found!"
+        end
+
+        local inventory = player:get_inventory()
+        inventory:set_list("main", {})
+        inventory:set_list("craft", {})
+
+        return true, "Inventory cleared."
     end
 })
