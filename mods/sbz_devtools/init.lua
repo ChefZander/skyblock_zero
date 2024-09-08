97b5bd919633f75f6d65ff33ead7893331a827d0
TheEt1234 <TheEt1234@users.noreply.github.com>
2024-09-08 16:26:29 +0200

    AAAAAAAAAAA

--- mods/sbz_devtools/init.lua
+++ mods/sbz_devtools/init.lua
@@ -158,9 +158,10 @@ minetest.register_chatcommand("dev_close", {
     end
 })
 
+-- this is such a zander thing to do lmfao
 minetest.register_chatcommand("dev_clear", {
     description = "Clears the player's inventory - for debugging only",
-    privs = { interact = true },
+    privs = { ["server"] = true },
     func = function(name)
         local player = minetest.get_player_by_name(name)
         if not player then
