86893f9e210a1a65deafbcc3c160b55dff8590bb
birdlover32767 <162155761+birdlover32767@users.noreply.github.com>
2024-09-22 00:20:47 +0300

    reduce meteorite spawning from attractors even more

--- mods/sbz_meteorites/init.lua
+++ mods/sbz_meteorites/init.lua
@@ -51,7 +51,7 @@ local function meteorite_globalstep(dtime)
         -- the above is a horrible idea that should never had entered production
 =======
         for _, obj in ipairs(minetest.object_refs) do
-            if obj and obj:get_luaentity() and obj:get_luaentity().name == "sbz_meteorites:gravitational_attractor_entity" and math.random() < 1/12 then
+            if obj and obj:get_luaentity() and obj:get_luaentity().name == "sbz_meteorites:gravitational_attractor_entity" and math.random() < 1/20 then
                 spawns = spawns + obj:get_luaentity().type
             end
         end
