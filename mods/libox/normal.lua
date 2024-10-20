4ca680df085e7502a080b9bc7efe0e4bae45b1f6
TheEt1234 <TheEt1234@users.noreply.github.com>
2024-10-20 21:14:52 +0200

    Ok maybe this should fix that bug for the final time

--- mods/libox/normal.lua
+++ mods/libox/normal.lua
@@ -27,7 +27,7 @@ function libox.normal_sandbox(def)
 
     f = function_wrap(f)
 
-    local old_hook = { debug.gethook() }
+    --    local old_hook = { debug.gethook() }
 
     debug.sethook(in_hook, "", def.hook_time or libox.default_hook_time)
     getmetatable("").__index = env.string
@@ -35,7 +35,7 @@ function libox.normal_sandbox(def)
         debug.sethook() -- fix a potential bug where someone can trigger a debug hook at just the right time for luanti to crash
         return error_handler(...)
     end)
-    debug.sethook(unpack(old_hook))
+    debug.sethook( --[[unpack(old_hook)--]])
 
 
     getmetatable("").__index = string
