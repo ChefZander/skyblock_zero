b7b5da8d23d7dbf5754c1d22cb9072a1a536bc73
theidealist <theidealistmusic@gmail.com>
2024-10-26 13:14:36 +0100

    still wanna do something with attractors increasing spawns somehow but I'll leave that for later

--- mods/sbz_meteorites/init.lua
+++ mods/sbz_meteorites/init.lua
@@ -41,14 +41,13 @@ local function meteorite_globalstep(dtime)
     time_since = time_since + dtime
     if time_since > 120 then
         time_since = 0
-        local spawns = math.random() < 0.25 and 1 or 0
+        if math.random() < 0.25 then spawn_meteorite() end
         --        for _, obj in ipairs(minetest.object_refs) do
         --            if obj and obj:get_luaentity() and obj:get_luaentity().name == "sbz_meteorites:gravitational_attractor_entity" and math.random() < 0.2 then
         --                spawns = spawns + obj:get_luaentity().type
         --            end
         --        end
         -- the above is a horrible idea that should never had entered production
-        if spawns > 0 then for _ = 1, spawns do spawn_meteorite() end end
     end
     storage:set_float("time_since_last_spawn", time_since)
 end
