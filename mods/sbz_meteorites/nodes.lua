d8c9fbb1013f48faa055b21f590b83f1e3a746de
birdlover32767 <162155761+birdlover32767@users.noreply.github.com>
2024-12-01 15:40:08 +0300

    matched meteorite nodes

--- mods/sbz_meteorites/nodes.lua
+++ mods/sbz_meteorites/nodes.lua
@@ -1,22 +1,21 @@
 minetest.register_node("sbz_meteorites:meteoric_matter", {
     description = "Meteoric Matter",
-    tiles = {"matter_blob.png^meteoric_overlay.png"},
+    tiles = { "matter_blob.png^meteoric_overlay.png" },
     paramtype = "light",
     light_source = 10,
-    groups = {matter=1, cracky=3},
+    groups = { matter = 1, cracky = 3 },
     drop = {
         max_items = 9,
         items = {
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=2, items={"sbz_resources:matter_dust"}},
-            {rarity=16, items={"sbz_resources:matter_blob"}},
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } },
+            { rarity = 2, items = { "sbz_resources:matter_dust" } }
         }
     },
     sounds = sbz_api.sounds.matter(),
@@ -24,23 +23,22 @@ minetest.register_node("sbz_meteorites:meteoric_matter", {
 
 minetest.register_node("sbz_meteorites:meteoric_antimatter", {
     description = "Meteoric Antimatter",
-    tiles = {"antimatter_blob.png^(meteoric_overlay.png^[invert:rgb)"},
+    tiles = { "antimatter_blob.png^(meteoric_overlay.png^[invert:rgb)" },
     paramtype = "light",
     light_source = 12,
-    groups = {antimatter=1, cracky=3},
+    groups = { antimatter = 1, cracky = 3 },
     drop = {
         max_items = 9,
         items = {
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=2, items={"sbz_resources:antimatter_dust"}},
-            {rarity=16, items={"sbz_resources:antimatter_blob"}},
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } },
+            { rarity = 2, items = { "sbz_resources:antimatter_dust" } }
         }
     },
     sounds = sbz_api.sounds.antimatter(),
@@ -48,34 +46,42 @@ minetest.register_node("sbz_meteorites:meteoric_antimatter", {
 
 minetest.register_node("sbz_meteorites:meteoric_emittrium", {
     description = "Meteoric Emittrium",
-    tiles = {"emitter.png^meteoric_overlay.png"},
+    tiles = { "emitter.png^meteoric_overlay.png" },
     paramtype = "light",
     light_source = 10,
-    groups = {matter=1, cracky=3},
+    groups = { matter = 1, cracky = 3 },
     drop = {
         max_items = 9,
         items = {
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=2, items={"sbz_resources:raw_emittrium"}},
-            {rarity=8, items={"sbz_resources:raw_emittrium 2"}},
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } },
+            { rarity = 2, items = { "sbz_resources:raw_emittrium" } }
         }
     },
     sounds = sbz_api.sounds.matter(),
 })
 
+local drops = sbz_api.crusher_drops
+local processed_drops = {}
+
+for k, v in ipairs(drops) do
+    processed_drops[#processed_drops + 1] = {
+        rarity = 16, items = { v }
+    }
+end
+
 minetest.register_node("sbz_meteorites:meteoric_metal", {
     description = "Meteoric Metal",
-    tiles = {"metal.png^meteoric_overlay.png"},
+    tiles = { "metal.png^meteoric_overlay.png" },
     paramtype = "light",
     light_source = 10,
-    groups = {matter=1, cracky=3},
+    groups = { matter = 1, cracky = 3 },
     drop = {
         max_items = 9,
         items = processed_drops
@@ -85,10 +91,10 @@ minetest.register_node("sbz_meteorites:meteoric_metal", {
 
 minetest.register_node("sbz_meteorites:neutronium", {
     description = "Neutronium",
-    tiles = {"neutronium.png"},
+    tiles = { "neutronium.png" },
     paramtype = "light",
     light_source = 4,
-    groups = { gravity = 300, matter = 1, charged = 1 },
+    groups = { matter = 1, charged = 1 },
     sounds = sbz_api.sounds.matter(),
 })
 minetest.register_node("sbz_meteorites:antineutronium", {
@@ -96,6 +102,6 @@ minetest.register_node("sbz_meteorites:antineutronium", {
     tiles = { "neutronium.png^[invert:rgb" },
     paramtype = "light",
     light_source = 8,
-    groups = { antigravity = 300, antimatter = 1, charged = 1 },
+    groups = { antimatter = 1, charged = 1 },
     sounds = sbz_api.sounds.antimatter(),
 })
