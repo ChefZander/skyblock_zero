ed3659f5feccd831d80a5ab46353cf45cd766133
TheEt1234 <TheEt1234@users.noreply.github.com>
2025-02-09 17:09:17 +0100

    Optimize starlight collector for servers

--- mods/sbz_power/generator.lua
+++ mods/sbz_power/generator.lua
@@ -268,32 +268,34 @@ minetest.register_craft({
         { "sbz_resources:matter_blob",   "sbz_resources:matter_blob",   "sbz_resources:matter_blob" }
     }
 })
-minetest.register_abm({
-    label = "Starlight Collector Particles",
-    nodenames = { "sbz_power:starlight_collector" },
-    interval = 1,
-    chance = 0.5,
-    action = function(pos, node, active_object_count, active_object_count_wider)
-        minetest.add_particlespawner({
-            amount = 2,
-            time = 1,
-            minpos = { x = pos.x - 0.5, y = pos.y + 0.5, z = pos.z - 0.5 },
-            maxpos = { x = pos.x + 0.5, y = pos.y + 1, z = pos.z + 0.5 },
-            minvel = { x = 0, y = -2, z = 0 },
-            maxvel = { x = 0, y = -1, z = 0 },
-            minacc = { x = 0, y = 0, z = 0 },
-            maxacc = { x = 0, y = 0, z = 0 },
-            minexptime = 1,
-            maxexptime = 1,
-            minsize = 0.5,
-            maxsize = 1.0,
-            collisiondetection = true,
-            vertical = false,
-            texture = "star.png",
-            glow = 10
-        })
-    end,
-})
+if not sbz_api.server_optimizations then
+    minetest.register_abm({
+        label = "Starlight Collector Particles",
+        nodenames = { "sbz_power:starlight_collector" },
+        interval = 1,
+        chance = 0.5,
+        action = function(pos, node, active_object_count, active_object_count_wider)
+            minetest.add_particlespawner({
+                amount = 2,
+                time = 1,
+                minpos = { x = pos.x - 0.5, y = pos.y + 0.5, z = pos.z - 0.5 },
+                maxpos = { x = pos.x + 0.5, y = pos.y + 1, z = pos.z + 0.5 },
+                minvel = { x = 0, y = -2, z = 0 },
+                maxvel = { x = 0, y = -1, z = 0 },
+                minacc = { x = 0, y = 0, z = 0 },
+                maxacc = { x = 0, y = 0, z = 0 },
+                minexptime = 1,
+                maxexptime = 1,
+                minsize = 0.5,
+                maxsize = 1.0,
+                collisiondetection = true,
+                vertical = false,
+                texture = "star.png",
+                glow = 10
+            })
+        end,
+    })
+end
 
 sbz_api.register_stateful_generator("sbz_power:antimatter_generator", {
     description = "Antimatter generator",
