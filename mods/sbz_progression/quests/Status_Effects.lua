3ef5eaa054dff8a3a9eea3a5845210b807a72d86
TheEt1234 <TheEt1234@users.noreply.github.com>
2025-02-27 17:06:56 +0100

    Change laser range, add playereffects

---
+++ mods/sbz_progression/quests/Status_Effects.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        type = "text",
+        title = "Questline: Status Effects",
+        text = [[
+This questline tells you all about status effects, *after* you get them.... :)
+        ]]
+    },
+    {
+        type = "secret",
+        title = "Attracted",
+        text = [[
+This effect is applied to you when you are holding a node with gravitational properties in your hand, near meteorite attractors/repulsors, the attraction is slower when you have more items in the stack that you are holding.
+During this effect, you can't move yourself, and you experience zero gravity.
+But you can still jump, and other knockback sources like explosions can still move you, and jetpack moves you too.
+
+And yes, you can go in orbit of the meteorite attractor, it just needs some practice.
+]],
+    }
+}
