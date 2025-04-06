1b6cf5e836d40655e96477dae012588c5b520dcb
TheEt1234 <TheEt1234@users.noreply.github.com>
2025-04-06 18:49:21 +0200

    Fix /playtime

--- mods/sbz_base/playtime_and_afk.lua
+++ mods/sbz_base/playtime_and_afk.lua
@@ -94,7 +94,7 @@ core.register_chatcommand("playtime", {
         if #param ~= 0 and param ~= " " then
             player = param
         end
-        core.chat_send_player(player, ("%s's playtime: %s\n%s's afk time: %s\n%s's afk and playtime: %s"):format(
+        core.chat_send_player(name, ("%s's playtime: %s\n%s's afk time: %s\n%s's afk and playtime: %s"):format(
             player, get_time(modstorage:get_int(player .. "_playtime")),
             player, get_time(modstorage:get_int(player .. "_afk_playtime")),
             player, get_time(modstorage:get_int(player .. "_playtime") + modstorage:get_int(player .. "_afk_playtime"))
