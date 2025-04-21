7ef4758136702baf1cf5c1ee73f985b37997f1fc
ChefZander <>
2025-04-21 14:08:14 +0200

    make optimize_pngs faster (4-core)

--- devtools/optimize_pngs.sh
+++ devtools/optimize_pngs.sh
@@ -1,2 +1,2 @@
 # from https://github.com/luanti-org/minetest_game/blob/master/utils/optimize_textures.sh
-find .. -name '*.png' -print0 | xargs -0 optipng -o7 -zm1-9 -nc -strip all -clobber
+find .. -name '*.png' -print0 | xargs -0  -n 1 -P 4 optipng -o7 -zm1-9 -nc -strip all -clobber
