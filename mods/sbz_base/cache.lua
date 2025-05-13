96ae15918002b2ca30d810b562873da4566de923
TheEt1234 <TheEt1234@users.noreply.github.com>
2025-05-13 15:26:11 +0200

    You can now insert colorium into the soil :3

--- mods/sbz_base/cache.lua
+++ mods/sbz_base/cache.lua
@@ -1,12 +1,13 @@
 sbz_api.all_caches = {}
 
-function sbz_api.make_cache(name, timer)
+function sbz_api.make_cache(name, timer, expire_on_jump_or_move)
     timer = timer or 0
     local cache = { data = {} }
     sbz_api.all_caches[name] = {
         cache = cache,
         timer_max = timer,
         timer = 0,
+        expire_on_jump_or_move = expire_on_jump_or_move,
     }
     return cache
 end
