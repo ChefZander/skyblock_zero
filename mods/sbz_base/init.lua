c70c634029c1776661e9f50f95a93fe04684b674
ChefZander <>
2025-09-28 20:13:50 +0200

    bump

--- mods/sbz_base/init.lua
+++ mods/sbz_base/init.lua
@@ -1,6 +1,6 @@
 ---@diagnostic disable-next-line: lowercase-global
 sbz_api = {
-    version = 38,
+    version = 39,
     is_version_dev = false,
     gravity = 9.8 / 2,
     server_optimizations = (core.settings:get 'sbz_server_mode' or 'auto'),
