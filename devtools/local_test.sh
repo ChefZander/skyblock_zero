0d7c5b8ac9b0c63c307312119625ebfd93fb283d
TheEt1234 <TheEt1234@users.noreply.github.com>
2026-01-17 10:19:00 +0100

    Remove some useless scripts in devtools/ and fix #184 (it was already fixed)

--- devtools/local_test.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm -it -v $(pwd)/..:/github/workspace \
-    -e INPUT_TEST_MODE=game \
-    -e INPUT_ENABLE_COVERAGE=true \
-    -e INPUT_ADDITIONAL_CONFIG="secure.trusted_mods=mtt,libox"\
-    ghcr.io/buckaroobanzay/mtt
