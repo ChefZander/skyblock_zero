c917fe00b9e2ae834245e2feb5e05130ee12dce7
TheEt1234 <TheEt1234@users.noreply.github.com>
2026-02-04 21:29:46 +0100

    Ooops

--- mods/sbz_progression/init.lua
+++ mods/sbz_progression/init.lua
@@ -171,6 +171,7 @@ local achievement_table = {
 
     ['sbz_power:manual_crafter'] = 'Manual Crafters',
     ['sbz_resources:nuclear_crafting_processor'] = 'Craftageddon',
+    ['sbz_area_containers:room_container'] = 'Room Containers',
 }
 
 minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv)
@@ -250,7 +251,6 @@ local achievement_on_dig_table = {
     ['sbz_bio:cleargrass_4'] = 'Cleargrass',
     ['sbz_bio:razorgrass_4'] = 'Razorgrass',
     ['sbz_bio:shockshroom_4'] = 'Shockshrooms',
-    ['sbz_area_containers:room_container'] = 'Room Containers',
 }
 
 minetest.register_on_player_inventory_action(function(player, action, inv, inv_info)
