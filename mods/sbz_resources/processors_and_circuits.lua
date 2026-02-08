13b6c640053b7af8bf3deb40ea8816ea7ddb01b6
TheEt1234 <TheEt1234@users.noreply.github.com>
2026-02-08 14:34:47 +0100

    Do the thing zander wanted me to do

--- mods/sbz_resources/processors_and_circuits.lua
+++ mods/sbz_resources/processors_and_circuits.lua
@@ -165,8 +165,8 @@ sbz_api.crafting_processor_stats = {
     ['sbz_resources:fast_crafting_processor'] = { crafts = 8, power = 20 },
     ['sbz_resources:very_fast_crafting_processor'] = { crafts = 32, power = 140 },
 
-    ['sbz_resources:needlessly_expensive_crafting_processor'] = { crafts = 128, power = 640 },
-    ['sbz_resources:omega_quantum_black_hole_whatever_crafting_processor'] = { crafts = 100000, power = 800 },
+    ['sbz_resources:extremely_fast_crafting_processor'] = { crafts = 128, power = 640 },
+    ['sbz_resources:instant_crafting_processor'] = { crafts = 100000, power = 800 },
 }
 
 core.register_craftitem('sbz_resources:simple_crafting_processor', {
@@ -215,15 +215,16 @@ minetest.register_craft {
     },
 }
 
-core.register_craftitem('sbz_resources:needlessly_expensive_crafting_processor', {
-    description = 'Needlessly Expensive Crafting Processor',
+core.register_craftitem('sbz_resources:extremely_fast_crafting_processor', {
+    description = 'Extremely Fast Crafting Processor',
     inventory_image = 'quantum_crafting_processor.png',
-    info_extra = "Crafts 128 items/s and uses 640Cj. You shouldn't need this, this item was made as a joke.",
+    info_extra = "Crafts 128 items/s and uses 640Cj. You shouldn't need this.",
 })
+core.register_alias('sbz_resources:needlessly_expensive_crafting_processor','sbz_resources:extremely_fast_crafting_processor')
 
 minetest.register_craft {
     type = 'shaped',
-    output = 'sbz_resources:needlessly_expensive_crafting_processor',
+    output = 'sbz_resources:extremely_fast_crafting_processor',
     recipe = {
         { 'sbz_resources:very_fast_crafting_processor', 'drawers:warpshroom_upgrade', 'sbz_resources:very_fast_crafting_processor' },
         { 'sbz_bio:giant_colorium_sapling', 'sbz_chem:thorium_crystal', 'sbz_bio:giant_colorium_sapling' },
@@ -231,21 +232,25 @@ minetest.register_craft {
     },
 }
 minetest.register_craft {
-    output = 'sbz_resources:omega_quantum_black_hole_whatever_crafting_processor',
+    output = 'sbz_resources:instant_crafting_processor',
     recipe = {
-        { 'sbz_resources:needlessly_expensive_crafting_processor', 'sbz_resources:needlessly_expensive_crafting_processor', 'sbz_resources:needlessly_expensive_crafting_processor', },
-        { 'sbz_resources:needlessly_expensive_crafting_processor', 'sbz_resources:needlessly_expensive_crafting_processor', 'sbz_resources:needlessly_expensive_crafting_processor', },
-        { 'sbz_resources:needlessly_expensive_crafting_processor', 'sbz_resources:needlessly_expensive_crafting_processor', 'sbz_resources:needlessly_expensive_crafting_processor', },
+        { 'sbz_resources:extremely_fast_crafting_processor', 'sbz_resources:extremely_fast_crafting_processor', 'sbz_resources:extremely_fast_crafting_processor', },
+        { 'sbz_resources:extremely_fast_crafting_processor', 'sbz_resources:extremely_fast_crafting_processor', 'sbz_resources:extremely_fast_crafting_processor', },
+        { 'sbz_resources:extremely_fast_crafting_processor', 'sbz_resources:extremely_fast_crafting_processor', 'sbz_resources:extremely_fast_crafting_processor', },
     },
 }
 
-
-core.register_craftitem('sbz_resources:omega_quantum_black_hole_whatever_crafting_processor', {
-    description = 'Omega Quantum Black Hole Whatever Crafting Processor',
-    inventory_image = 'creative_crafting_processor.png',
-    info_extra = 'Crafts 100000 items/s, consumes 800Cj.\nThis item is a joke. You should not try to get it.',
-})
 -- stylua: ignore end
+
+core.register_craftitem('sbz_resources:instant_crafting_processor', {
+    description = 'Instant Crafting Processor',
+    inventory_image = 'creative_crafting_processor.png',
+    info_extra = 'Crafts 100000 items/s, consumes 800Cj.\nThe crafting recipe is a joke. You should not try to get it... but if you want to',
+})
+core.register_alias(
+    'sbz_resources:omega_quantum_black_hole_whatever_crafting_processor',
+    'sbz_resources:instant_crafting_processor'
+)
 
 -- deprecated stuff
 core.register_craftitem('sbz_resources:mosfet', {
