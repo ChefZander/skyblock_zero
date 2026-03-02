54bdde698b29802d2285e96e642fbb0995f52884
Abigail Read <AbbyReadMail@gmail.com>
2026-03-02 11:13:11 -0800

    Simplify license copy (remove repeat text)

--- mods/drawers/init.lua
+++ mods/drawers/init.lua
@@ -1,6 +1,8 @@
 --[[
 Minetest Mod Storage Drawers - A Mod adding storage drawers
 
+Copyright (C) 2026 Skyblock: Zero
+Copyright (C) 2021 by Pandorabox
 Copyright (C) 2017-2020 Linus Jahn <lnj@kaidan.im>
 
 MIT License
@@ -158,33 +160,8 @@ core.register_craft({
 	}
 })
 
+--Section below modified as of 2021 by Pandorabox
 
---[[
-(Btw i modified the code below slightly)
-The code under this line is licensed under:
-
-MIT License
-
-Copyright (c) 2021 Pandorabox
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-]]
 minetest.register_chatcommand("drawers_fix", {
 	description = "recreates the drawer-visuals in your area",
 	func = function(name)
