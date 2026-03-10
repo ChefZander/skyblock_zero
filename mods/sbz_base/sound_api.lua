b7cabe8e3d011f23cc7619683239b8b52c69e55d
Abigail Read <AbbyReadMail@gmail.com>
2026-03-09 20:18:40 -0700

    Start gutting the sound api
    I want to make it clearer and more intentional when you set sounds on nodes and items and things.  To do that, I'm taking out the rigmarole that affords defaults that we haven't even consistently used up to this point.

--- mods/sbz_base/sound_api.lua
+++ mods/sbz_base/sound_api.lua
@@ -1,88 +1,26 @@
--- very similar to xcompat's sound api
--- sbz_api.sounds.node_sound_x_defaults(inp)
-
-local sounds = setmetatable({}, {
-    __index = function(t, k)
-        return rawget(t, k) or function(input)
-            return table.override({}, input)
-        end
-    end,
-})
----@type table<string, fun(inp:table?)>
-sbz_api.sounds = sounds
-
-local function register_sound_function(name, sounds_to_use)
-    sounds[name] = function(input)
-        return table.override(sounds_to_use, input)
-    end
-end
-
-local function g1(x)
-    return { name = x, gain = 1 }
-end
-local function csound(x, g)
-    return { name = x, gain = g }
-end
-
-register_sound_function('matter', { -- also could be node_stone_defaults
-    footstep = csound('step', 2),
-    place = csound('step', 2),
-})
-register_sound_function('metal', {
-    footstep = csound('step', 2),
-    place = csound('step', 2),
-})
-
-register_sound_function('glass', {
-    footstep = csound('step', 2),
-    place = csound('step', 2),
-})
-
-register_sound_function('dirt', {
-    footstep = csound('step', 2),
-    place = csound('step', 2),
-})
-
-register_sound_function('tree', {
-    footstep = csound('step', 2),
-    place = csound('step', 2),
-})
-
-register_sound_function('leaves', {
-    --    footstep = csound("step", 2),
-    --    place = csound("step", 2),
-})
-
-register_sound_function('antimatter', {
-    footstep = g1 'antistep',
-    place = g1 'antistep',
-})
-
-register_sound_function('strange', {
-    footstep = g1 'antistep',
-    place = g1 'antistep',
-})
-
+--[[ 
 register_sound_function('machine', {
     footstep = csound('step', 2),
     place = g1 'machine_build',
     rightclick = g1 'machine_open',
 })
-
+ ]]
 -- allow for a rightclick parameter too, so that its less annoying
-minetest.register_on_mods_loaded(function()
-    for k, v in pairs(minetest.registered_nodes) do
+core.register_on_mods_loaded(function()
+    for k, v in pairs(core.registered_nodes) do
+        -- If it has a sounds.rightclick specified...
         if v.sounds and v.sounds.rightclick then
+            -- Take what is specified and save it as old_rightclick
             local old_rightclick = v.on_rightclick or function(pos, node, clicker, itemstack, pointed_thing) end
             local function new_rightclick(pos, node, clicker, stack, pointed)
-                if minetest.get_meta(pos):get_string 'formspec' ~= '' then
-                    minetest.sound_play(v.sounds.rightclick, {
+                if core.get_meta(pos):get_string 'formspec' ~= '' then
+                    core.sound_play(v.sounds.rightclick, {
                         pos = pos,
                     })
                 end
                 return old_rightclick(pos, node, clicker, stack, pointed)
             end
-            minetest.override_item(k, {
+            core.override_item(k, {
                 on_rightclick = new_rightclick,
             })
         end
