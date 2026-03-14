61cd96b9d4e7a2c4f15ded1e5e38c2b947928f78
Abigail Read <AbbyReadMail@gmail.com>
2026-03-14 07:30:18 -0700

    Make my linter stop crying

--- mods/metatool/metatool/formspec.lua
+++ mods/metatool/metatool/formspec.lua
@@ -1,5 +1,5 @@
 local S = metatool.S
-local formspec_escape = minetest.formspec_escape
+local formspec_escape = core.formspec_escape
 
 local function formspec_content(value, default)
 	return formspec_escape(value or default or "")
@@ -27,8 +27,8 @@ setmetatable(Form, {
 		local obj = {
 			width = def and def.width or 8,
 			height = def and def.height or 8,
-			xspacing = def and (def.xspacing or def.spacing) or 0.1,
-			yspacing = def and (def.yspacing or def.spacing) or 0.1,
+			x_spacing = def and (def.x_spacing or def.spacing) or 0.1,
+			y_spacing = def and (def.y_spacing or def.spacing) or 0.1,
 			elements = def and def.elements or {},
 		}
 		obj.formspec = ("formspec_version[3]size[%s;]"):format(fmt_rect(obj.width, obj.height))
@@ -37,14 +37,14 @@ setmetatable(Form, {
 	end
 })
 
-function Form:get_rect(x, y, w, h, xcount, xindex, ycount, yindex, paddingtop)
-	local sx = self.xspacing
-	local sy = self.yspacing
-	local cx = xcount or 1
-	local ix = xindex or 1
-	local cy = ycount or 1
-	local iy = yindex or 1
-	local pt = paddingtop or 0
+function Form:get_rect(x, y, w, h, x_count, x_index, y_count, y_index, padding_top)
+	local sx = self.x_spacing
+	local sy = self.y_spacing
+	local cx = x_count or 1
+	local ix = x_index or 1
+	local cy = y_count or 1
+	local iy = y_index or 1
+	local pt = padding_top or 0
 	w = w or ((self.width - ((cx + 1) * sx)) / cx)
 	h = h or ((self.height - ((cy + 1) * sy)) / cy)
 	if cx > 1 then
@@ -62,7 +62,7 @@ function Form:raw(element)
 	return self
 end
 
--- NOTE: Unstable function signature and `def` format. High probablility for breaking changes soon.
+-- NOTE: Unstable function signature and `def` format. High probability for breaking changes soon.
 -- Formspec API is not included in stable features and breaking changes will not change major version.
 -- def.columns and def.values required
 -- everything else is optional
@@ -77,9 +77,9 @@ function Form:table(def)
 		table.insert(columns, "text")
 		table.insert(values, def.columns[i])
 	end
-	local ccount = #def.columns
+	local col_count = #def.columns
 	for _, v in ipairs(def.values) do
-		for i = 1, ccount do
+		for i = 1, col_count do
 			table.insert(values, v[i] and formspec_escape(v[i]) or "")
 		end
 	end
@@ -87,7 +87,7 @@ function Form:table(def)
 	local background = def.background and (";background=%s"):format(def.background) or ""
 	local highlight = def.highlight and (";highlight=%s"):format(def.highlight) or ""
 	local color = def.color and (";color=%s"):format(def.color) or ""
-	local x, y, w, h = self:get_rect(def.x, def.y, def.w, def.h, def.xcount, def.xidx, def.ycount, def.yidx, pt)
+	local x, y, w, h = self:get_rect(def.x, def.y, def.w, def.h, def.x_count, def.x_index, def.y_count, def.y_index, pt)
 	table.insert(self.elements,
 		(def.label and ("label[%s;%s]"):format(fmt_rect(x + 0.1, y - 0.2), def.label)) ..
 		("tableoptions[border=false%s%s%s]"):format(background, highlight, color) ..
@@ -97,7 +97,7 @@ function Form:table(def)
 	return self
 end
 
--- NOTE: Unstable function signature and `def` format. High probablility for breaking changes soon.
+-- NOTE: Unstable function signature and `def` format. High probability for breaking changes soon.
 -- Formspec API is not included in stable features and breaking changes will not change major version.
 -- NOTE: Image button feature unstable, property names will most probably change
 function Form:button(def)
@@ -105,7 +105,7 @@ function Form:button(def)
 		return self
 	end
 	local label = formspec_content(def.label, def.name)
-	local x, y, w, h = self:get_rect(def.x, def.y, def.w, def.h, def.xcount, def.xidx, def.ycount, def.yidx)
+	local x, y, w, h = self:get_rect(def.x, def.y, def.w, def.h, def.x_count, def.x_index, def.y_count, def.y_index)
 	local properties
 	if def.texture1 then
 		local t1 = def.texture1 .. (def.modifier and formspec_escape(def.modifier) or "")
@@ -120,7 +120,7 @@ function Form:button(def)
 	return self
 end
 
--- NOTE: Unstable function signature and `def` format. High probablility for breaking changes soon.
+-- NOTE: Unstable function signature and `def` format. High probability for breaking changes soon.
 -- Formspec API is not included in stable features and breaking changes will not change major version.
 function Form:field(def)
 	if not def.name then
@@ -128,7 +128,7 @@ function Form:field(def)
 	end
 	local label = formspec_content(def.label, def.name)
 	local default = formspec_content(def.default)
-	local x, y, w, h = self:get_rect(def.x, def.y, def.w, def.h, def.xcount, def.xidx, def.ycount, def.yidx)
+	local x, y, w, h = self:get_rect(def.x, def.y, def.w, def.h, def.x_count, def.x_index, def.y_count, def.y_index)
 	table.insert(self.elements,
 		("field[%s;%s;%s;%s]"):format(fmt_rect(x, y, w, h), def.name, label, default)
 	)
@@ -137,7 +137,7 @@ end
 
 function Form:label(def)
 	local label = formspec_content(def.label, def.name)
-	local x, y = self:get_rect(def.x, def.y, def.w, def.h, def.xcount, def.xidx, def.ycount, def.yidx)
+	local x, y = self:get_rect(def.x, def.y, def.w, def.h, def.x_count, def.x_index, def.y_count, def.y_index)
 	table.insert(self.elements,
 		("label[%s;%s]"):format(fmt_rect(x, y), label)
 	)
@@ -156,7 +156,7 @@ metatool.form = {
 -- container for form handler callback methods
 metatool.form.handlers = {}
 
-local function get_formname(name)
+local function get_form_name(name)
 	if not name then
 		return
 	end
@@ -179,12 +179,12 @@ end
 -- Temporary storage for form data references
 -- Links are created after(!) form is constructed
 -- Links are destroyed after receiving fields and data is lost if not maintained by caller
-local formdata = {}
+local form_data = {}
 
-local function destroy_formdata(player)
+local function destroy_form_data(player)
 	local name = type(player) == "userdata" and player:get_player_name() or player
 	if name then
-		formdata[name] = nil
+		form_data[name] = nil
 	end
 end
 
@@ -193,42 +193,42 @@ function metatool.form.register_global_handler()
 	if not global_form_handler_registered then
 		global_form_handler_registered = true
 		print("metatool.form.register_global_handler Registering global formspec handler for Metatool")
-		minetest.register_on_player_receive_fields(metatool.form.on_receive)
-		minetest.register_on_leaveplayer(destroy_formdata)
+		core.register_on_player_receive_fields(metatool.form.on_receive)
+		core.register_on_leaveplayer(destroy_form_data)
 	end
 end
 
-function metatool.form.on_receive(player, formname, fields)
-	local matcher = formname:gmatch("([^~]+)")
-	formname = matcher()
-	if not has_form(formname) then
+function metatool.form.on_receive(player, form_name, fields)
+	local matcher = form_name:gmatch("([^~]+)")
+	form_name = matcher()
+	if not has_form(form_name) then
 		-- form handler does not exist
 		return
 	end
-	if fields and metatool.form.handlers[formname].on_receive then
+	if fields and metatool.form.handlers[form_name].on_receive then
 		local playername = player:get_player_name()
-		local data = formdata[playername]
+		local data = form_data[playername]
 		local name = data and data.name
-		if name ~= formname then
-			minetest.chat_send_player(playername, "Bug: Form name mismatch: " .. formname)
+		if name ~= form_name then
+			core.chat_send_player(playername, "Bug: Form name mismatch: " .. form_name)
 			return true
 		end
 		local secure = data and data.nonce == matcher()
 		local result
 		-- call actual form handler
 		if secure then
-			result = metatool.form.handlers[formname].on_receive(player, fields, data.data, secure)
+			result = metatool.form.handlers[form_name].on_receive(player, fields, data.data, secure)
 		elseif not fields.quit then
-			minetest.chat_send_player(playername, "Bug: Invalid security token for form " .. formname)
+			core.chat_send_player(playername, "Bug: Invalid security token for form " .. form_name)
 		end
 		if result and not fields.quit then
-			minetest.close_formspec(playername, formname)
+			core.close_formspec(playername, form_name)
 		end
 		if fields.quit and data then
-			formdata[playername] = nil
+			form_data[playername] = nil
 		elseif result == false then
 			-- Update formspec and send to player
-			metatool.form.show(player, formname, data.data)
+			metatool.form.show(player, form_name, data.data)
 		elseif type(result) == "string" then
 			-- Switch formspec and send to player
 			metatool.form.show(player, result, data.data)
@@ -238,57 +238,57 @@ function metatool.form.on_receive(player, formname, fields)
 	return true
 end
 
-function metatool.form.on_create(player, formname, data)
-	if has_form(formname) and type(metatool.form.handlers[formname].on_create) == "function" then
+function metatool.form.on_create(player, form_name, data)
+	if has_form(form_name) and type(metatool.form.handlers[form_name].on_create) == "function" then
 		-- Use callback to get formspec definition
-		return metatool.form.handlers[formname].on_create(player, data)
+		return metatool.form.handlers[form_name].on_create(player, data)
 	end
 end
 
 -- on_create should be function that returns Form object.
 -- on_receive is function that receives fields when form is submitted, can be nil.
-function metatool.form.register_form(formname, formdef)
-	local name = get_formname(formname)
+function metatool.form.register_form(form_name, form_def)
+	local name = get_form_name(form_name)
 	if not name then
-		print(S("metatool.form.register_form Registration failed, invalid formname: %s", formname))
+		print(S("metatool.form.register_form Registration failed, invalid form_name: %s", form_name))
 		return
 	end
-	if type(formdef) ~= "table" then
-		print(S("metatool.form.register_form Registration failed, invalid formdef type: %s", type(formdef)))
+	if type(form_def) ~= "table" then
+		print(S("metatool.form.register_form Registration failed, invalid form_def type: %s", type(form_def)))
 		return
 	end
 	metatool.form.register_global_handler()
 	print(S("metatool.form.register_form Registering form: %s", name))
 	metatool.form.handlers[name] = {
-		on_create = formdef.on_create,
-		on_receive = formdef.on_receive,
+		on_create = form_def.on_create,
+		on_receive = form_def.on_receive,
 	}
 end
 
 -- display formspec to player, if optional data variable is given then it will be
 -- saved and forwarded to on_create and on_receive form handler callback functions.
-function metatool.form.show(player, formname, data)
-	local name = get_formname(formname)
+function metatool.form.show(player, form_name, data)
+	local name = get_form_name(form_name)
 	if has_form(name) then
 		-- Construct form
 		local nonce = generate_nonce()
 		local form = metatool.form.on_create(player, name, data)
 		local playername = player:get_player_name()
 		if type(form) ~= "table" then
-			minetest.chat_send_player(playername, "Bug: Attempt to open invalid form " .. formname)
+			core.chat_send_player(playername, "Bug: Attempt to open invalid form " .. form_name)
 			return
 		end
 		if metatool.form.handlers[name].on_receive then
 			-- Store temporary data ref if form input is processed somehow
-			formdata[playername] = {
+			form_data[playername] = {
 				name = name,
 				nonce = nonce,
 				data = data,
 			}
 		end
 		-- Show form to player
-		minetest.show_formspec(playername, name .. "~" .. nonce, form:render())
+		core.show_formspec(playername, name .. "~" .. nonce, form:render())
 	else
-		minetest.chat_send_player(player:get_player_name(), "Bug: Metatool form does not exist " .. formname)
+		core.chat_send_player(player:get_player_name(), "Bug: Metatool form does not exist " .. form_name)
 	end
 end
