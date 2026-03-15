b1c23124e48a8a0dadc3f3a145b68cbb8e53fbfe
f3mboy-phoenix <f3mboy.phoenix@gmail.com>
2026-03-14 22:03:14 -0400

    force credits quest to be last

--- mods/sbz_progression/quests.lua
+++ mods/sbz_progression/quests.lua
@@ -38,65 +38,68 @@ local t0 = core.get_us_time()
 table.foreach(quest_files, function(name)
     sbz_api.quests_from_file_md(minetest.get_modpath 'sbz_progression' .. '/quests/' .. name .. '.md')
 end, true)
-sbz_api.register_quest({
-    ["type"] = "secret",
-    ["istoplevel"] = true,
-    ["title"] = "Credits",
-    ["text"] = 
-    "Thank you for playing Skyblock: Zero. "
-    .. "You demonstrated extreme logical thinking skills, planning skills, and invested probably at least a couple of hours into our game. "
-    .. "We hope you enjoyed this current version, because there will be more coming. "
-    .. "- Zander\n"
-    .. "\n"
 
-    .. "Below is a list of all the people who helped work on the game, optionally, along with a quote by the respective developer.\n"
-    -- Note to developers: Feel free to add yourself to the list if you're new, along with a quote. 
-    -- If you're already on the list, feel free to edit your quote as well. (But not other people's)
+core.after(0, function ()
+    sbz_api.register_quest({
+        ["type"] = "secret",
+        ["istoplevel"] = true,
+        ["title"] = "Credits",
+        ["text"] = 
+        "Thank you for playing Skyblock: Zero. "
+        .. "You demonstrated extreme logical thinking skills, planning skills, and invested probably at least a couple of hours into our game. "
+        .. "We hope you enjoyed this current version, because there will be more coming. "
+        .. "- Zander\n"
+        .. "\n"
 
-    .. "Developers & Contributors\n"
-    .. "- Zander \"Removing Global Power is the worst thing to have happened to Skyblock: Zero\"\n"
-    .. "- frogTheSecond\n"
-    .. "- theidealist\n"
-    .. "- birdlover32767\n"
-    .. "- ACorp \"0 coolant - not great, not terrible\"\n"
-    .. "- lnee\n"
-    .. "- The4codeblocks\n"
-    .. "- watilin\n"
-    .. "- Aredron\n"
-    .. "- 0je\n"
-    .. "- JustAPotota\n"
-    .. "- Kycilak\n"
-    .. "- cx384\n"
-    .. "- AbbyRead\n"
-    .. "- jack-obrien\n"
-    .. "- gamefreq0\n"
-    .. "\n"
+        .. "Below is a list of all the people who helped work on the game, optionally, along with a quote by the respective developer.\n"
+        -- Note to developers: Feel free to add yourself to the list if you're new, along with a quote. 
+        -- If you're already on the list, feel free to edit your quote as well. (But not other people's)
 
-    .. "Submodule Contributors\n"
-    .. "- SmallJoker\n"
-    .. "- SwissalpS\n"
-    .. "- lumberjackgames\n"
-    .. "- Emojigit\n"
-    .. "\n"
+        .. "Developers & Contributors\n"
+        .. "- Zander \"Removing Global Power is the worst thing to have happened to Skyblock: Zero\"\n"
+        .. "- frogTheSecond\n"
+        .. "- theidealist\n"
+        .. "- birdlover32767\n"
+        .. "- ACorp \"0 coolant - not great, not terrible\"\n"
+        .. "- lnee\n"
+        .. "- The4codeblocks\n"
+        .. "- watilin\n"
+        .. "- Aredron\n"
+        .. "- 0je\n"
+        .. "- JustAPotota\n"
+        .. "- Kycilak\n"
+        .. "- cx384\n"
+        .. "- AbbyRead\n"
+        .. "- jack-obrien\n"
+        .. "- gamefreq0\n"
+        .. "\n"
 
-    .. "Translators\n"
-    .. "There's not been any work to translate the game outside of English. Maybe your name will one day show up here?\n"
-    .. "\n"
+        .. "Submodule Contributors\n"
+        .. "- SmallJoker\n"
+        .. "- SwissalpS\n"
+        .. "- lumberjackgames\n"
+        .. "- Emojigit\n"
+        .. "\n"
 
-    .. "Menu & In-Game Music\n"
-    .. "- 'Tragic ambient main menu' by Brandon Morris (Menu Music)\n"
-    .. "- 'Cave Theme' by Brandon Morris\n"
-    .. "- 'Background space track' by yd\n"
-    .. "- 'Bleeding Out' by Brandon Morris\n"
-    .. "- 'Factory Ambiance' by yd\n"
-    .. "- 'Galactic Temple' by yd\n"
-    .. "- 'A Choice With Many Regrets' by Tsorthan Grove"
-    .. "\n"
+        .. "Translators\n"
+        .. "There's not been any work to translate the game outside of English. Maybe your name will one day show up here?\n"
+        .. "\n"
 
-    .. "Skybox was made by StumpyStrust.\n"
-    .. "\n"
+        .. "Menu & In-Game Music\n"
+        .. "- 'Tragic ambient main menu' by Brandon Morris (Menu Music)\n"
+        .. "- 'Cave Theme' by Brandon Morris\n"
+        .. "- 'Background space track' by yd\n"
+        .. "- 'Bleeding Out' by Brandon Morris\n"
+        .. "- 'Factory Ambiance' by yd\n"
+        .. "- 'Galactic Temple' by yd\n"
+        .. "- 'A Choice With Many Regrets' by Tsorthan Grove"
+        .. "\n"
 
-    .. "You've finished the Questbook, but the game has only just begun.\n"
-    .. "",
-})
+        .. "Skybox was made by StumpyStrust.\n"
+        .. "\n"
+
+        .. "You've finished the Questbook, but the game has only just begun.\n"
+        .. "",
+    })
+end)
 core.log('action', 'Loading quests from markdown took: ' .. ((core.get_us_time() - t0) / 1000) .. 'ms')
