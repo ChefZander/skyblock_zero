580ea68956cd1c9129425a3eecff4d869e45b3a2
Abigail Read <AbbyReadMail@gmail.com>
2026-03-15 03:51:18 -0700

    Refactor jumpdrive-related recipes

--- mods/jumpdrive/crafts.lua
+++ mods/jumpdrive/crafts.lua
@@ -1,35 +1,58 @@
-minetest.register_craft({
-    output = 'jumpdrive:engine',
-    recipe = {
-        { 'jumpdrive:backbone',    'sbz_power:very_advanced_battery', 'jumpdrive:backbone' },
-        { 'jumpdrive:warp_device', 'sbz_meteorites:neutronium',       'jumpdrive:warp_device' },
-        { 'jumpdrive:warp_device', 'sbz_power:antimatter_generator',  'jumpdrive:warp_device' },
-    }
-})
+do -- Engine recipe scope
+    local Engine = 'jumpdrive:engine'
+    local Ba = 'jumpdrive:backbone'
+    local VA = 'sbz_power:very_advanced_battery'
+    local WD = 'jumpdrive:warp_device'
+    local Ne = 'sbz_meteorites:neutronium'
+    local AG = 'sbz_power:antimatter_generator'
+    core.register_craft({
+        output = Engine,
+        recipe = {
+            { Ba, VA, Ba },
+            { WD, Ne, WD },
+            { WD, AG, WD },
+        }
+    })
+end
 
-minetest.register_craft({
-    output = 'jumpdrive:backbone',
-    recipe = {
-        { 'sbz_chem:titanium_block', 'sbz_power:reactor_shell', 'sbz_chem:titanium_block' },
-        { 'sbz_power:reactor_shell', 'sbz_power:reactor_shell', 'sbz_power:reactor_shell' },
-        { 'sbz_chem:titanium_block', 'sbz_power:reactor_shell', 'sbz_chem:titanium_block' }
-    }
-})
+do -- Backbone recipe scope
+    local Backbone = 'jumpdrive:backbone'
+    local TB = 'sbz_chem:titanium_block'
+    local RS = 'sbz_power:reactor_shell'
+    core.register_craft({
+        output = Backbone,
+        recipe = {
+            { TB, RS, TB },
+            { RS, RS, RS },
+            { TB, RS, TB },
+        }
+    })
+end
 
-minetest.register_craft({
-    output = 'jumpdrive:warp_device',
-    recipe = {
-        { 'sbz_resources:warp_crystal', 'sbz_resources:warp_crystal',         'sbz_resources:warp_crystal' },
-        { 'sbz_resources:warp_crystal', 'sbz_power:antimatter_generator_off', 'sbz_resources:warp_crystal' },
-        { 'sbz_resources:warp_crystal', 'sbz_resources:warp_crystal',         'sbz_resources:warp_crystal' }
-    }
-})
+do -- Warp Device recipe scope
+    local Warp_Device = 'jumpdrive:warp_device'
+    local WC = 'sbz_resources:warp_crystal'
+    local AG = 'sbz_power:antimatter_generator_off'
+    core.register_craft({
+        output = Warp_Device,
+        recipe = {
+            { WC, WC, WC },
+            { WC, AG, WC },
+            { WC, WC, WC },
+        }
+    })
+end
 
-minetest.register_craft({
-    output = 'jumpdrive:fleet_controller',
-    recipe = {
-        { 'jumpdrive:backbone', 'jumpdrive:backbone',       'jumpdrive:backbone' },
-        { 'jumpdrive:backbone', 'sbz_logic:lua_controller', 'jumpdrive:backbone' },
-        { 'jumpdrive:backbone', 'jumpdrive:backbone',       'jumpdrive:backbone' }
-    }
-})
+do -- Fleet Controller recipe scope
+    local Fleet_Controller = 'jumpdrive:fleet_controller'
+    local Ba = 'jumpdrive:backbone'
+    local LC = 'sbz_logic:lua_controller'
+    core.register_craft({
+        output = Fleet_Controller,
+        recipe = {
+            { Ba, Ba, Ba },
+            { Ba, LC, Ba },
+            { Ba, Ba, Ba },
+        }
+    })
+end
