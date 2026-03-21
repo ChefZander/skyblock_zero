b452e4c552d1c56bface854ee47415204896b4fa
ChefZander <60350322+ChefZander@users.noreply.github.com>
2026-03-21 15:21:28 +0100

    bump properly

--- mods/sbz_base/init.lua
+++ mods/sbz_base/init.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable-next-line: lowercase-global
 sbz_api = {
-    version = 43,
-    is_version_dev = false,
+    version = 45,
+    is_version_dev = true,
     gravity = 9.8 / 2,
     server_optimizations = (core.settings:get 'sbz_server_mode' or 'auto'),
     deg2rad = math.pi / 180,
