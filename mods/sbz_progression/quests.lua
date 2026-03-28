648da7472771de3b54c5d5715051ae6d4b2059dc
Abigail Read <AbbyReadMail@gmail.com>
2026-03-28 15:14:29 -0700

    Index quests and fix credits text indentation

--- mods/sbz_progression/quests.lua
+++ mods/sbz_progression/quests.lua
@@ -1,7 +1,11 @@
 quests = {}
 
+-- Index for faster lookups when patching-in the translations
+local quest_id_index = {}
+
 function sbz_api.register_quest(def)
     quests[#quests + 1] = def
+    quest_id_index[def.id] = #quests
 end
 
 local quest_files = {
@@ -52,60 +56,60 @@ sbz_api.register_quest({
     ["istoplevel"] = true,
     ["title"] = "Credits",
     ["text"] =
-    "Thank you for playing Skyblock: Zero. "
-    .. "You demonstrated extreme logical thinking skills, planning skills, and invested probably at least a couple of hours into our game. "
-    .. "We hope you enjoyed this current version, because there will be more coming. "
-    .. "- Zander\n"
-    .. "\n"
+        "Thank you for playing Skyblock: Zero. "
+        .. "You demonstrated extreme logical thinking skills, planning skills, and invested probably at least a couple of hours into our game. "
+        .. "We hope you enjoyed this current version, because there will be more coming. "
+        .. "- Zander\n"
+        .. "\n"
 
-    .. "Below is a list of all the people who helped work on the game, optionally, along with a quote by the respective developer.\n"
-    -- Note to developers: Feel free to add yourself to the list if you're new, along with a quote.
-    -- If you're already on the list, feel free to edit your quote as well. (But not other people's)
+        .. "Below is a list of all the people who helped work on the game, optionally, along with a quote by the respective developer.\n"
+        -- Note to developers: Feel free to add yourself to the list if you're new, along with a quote.
+        -- If you're already on the list, feel free to edit your quote as well. (But not other people's)
 
-    .. "Developers & Contributors\n"
-    .. "- Zander \"Removing Global Power is the worst thing to have happened to Skyblock: Zero\"\n"
-    .. "- frogTheSecond\n"
-    .. "- theidealist\n"
-    .. "- birdlover32767\n"
-    .. "- ACorp \"0 coolant - not great, not terrible\"\n"
-    .. "- lnee\n"
-    .. "- The4codeblocks\n"
-    .. "- watilin\n"
-    .. "- Aredron\n"
-    .. "- 0je\n"
-    .. "- JustAPotota\n"
-    .. "- Kycilak\n"
-    .. "- cx384\n"
-    .. "- AbbyRead\n"
-    .. "- jack-obrien\n"
-    .. "- gamefreq0\n"
-    .. "\n"
+        .. "Developers & Contributors\n"
+        .. "- Zander \"Removing Global Power is the worst thing to have happened to Skyblock: Zero\"\n"
+        .. "- frogTheSecond\n"
+        .. "- theidealist\n"
+        .. "- birdlover32767\n"
+        .. "- ACorp \"0 coolant - not great, not terrible\"\n"
+        .. "- lnee\n"
+        .. "- The4codeblocks\n"
+        .. "- watilin\n"
+        .. "- Aredron\n"
+        .. "- 0je\n"
+        .. "- JustAPotota\n"
+        .. "- Kycilak\n"
+        .. "- cx384\n"
+        .. "- AbbyRead\n"
+        .. "- jack-obrien\n"
+        .. "- gamefreq0\n"
+        .. "\n"
 
-    .. "Submodule Contributors\n"
-    .. "- SmallJoker\n"
-    .. "- SwissalpS\n"
-    .. "- lumberjackgames\n"
-    .. "- Emojigit\n"
-    .. "\n"
+        .. "Submodule Contributors\n"
+        .. "- SmallJoker\n"
+        .. "- SwissalpS\n"
+        .. "- lumberjackgames\n"
+        .. "- Emojigit\n"
+        .. "\n"
 
-    .. "Translators\n"
-    .. "There's not been any work to translate the game outside of English. Maybe your name will one day show up here?\n"
-    .. "\n"
+        .. "Translators\n"
+        .. "There's not been any work to translate the game outside of English. Maybe your name will one day show up here?\n"
+        .. "\n"
 
-    .. "Menu & In-Game Music\n"
-    .. "- 'Tragic ambient main menu' by Brandon Morris (Menu Music)\n"
-    .. "- 'Cave Theme' by Brandon Morris\n"
-    .. "- 'Background space track' by yd\n"
-    .. "- 'Bleeding Out' by Brandon Morris\n"
-    .. "- 'Factory Ambiance' by yd\n"
-    .. "- 'Galactic Temple' by yd\n"
-    .. "- 'A Choice With Many Regrets' by Tsorthan Grove"
-    .. "\n"
+        .. "Menu & In-Game Music\n"
+        .. "- 'Tragic ambient main menu' by Brandon Morris (Menu Music)\n"
+        .. "- 'Cave Theme' by Brandon Morris\n"
+        .. "- 'Background space track' by yd\n"
+        .. "- 'Bleeding Out' by Brandon Morris\n"
+        .. "- 'Factory Ambiance' by yd\n"
+        .. "- 'Galactic Temple' by yd\n"
+        .. "- 'A Choice With Many Regrets' by Tsorthan Grove"
+        .. "\n"
 
-    .. "Skybox was made by StumpyStrust.\n"
-    .. "\n"
+        .. "Skybox was made by StumpyStrust.\n"
+        .. "\n"
 
-    .. "You've finished the Questbook, but the game has only just begun.\n"
-    .. "",
+        .. "You've finished the Questbook, but the game has only just begun.\n"
+        .. "",
 })
 core.log('action', 'Loading quests from markdown took: ' .. ((core.get_us_time() - t0) / 1000) .. 'ms')
